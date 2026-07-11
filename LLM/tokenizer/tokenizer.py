2399568199592196ccba0772a82f26e4234a853b
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-11 16:27:15 +0530

    Removed code for demo.py
    accidentally entered the wrong code lol

--- LLM/tokenizer/tokenizer.py
+++ LLM/tokenizer/tokenizer.py
@@ -68,16 +68,3 @@ class Tokenizer:
             self.id_to_word[i]
             for i in ids
         )
-
-with open("data.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-
-tok = Tokenizer()
-
-tok.train(text)
-
-encoded = tok.encode("Hello AI!")
-
-print(encoded)
-
-print(tok.decode(encoded))
