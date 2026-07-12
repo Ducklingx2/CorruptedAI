92b2db55733e0a84c2c32c7767309b394ae65c70
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-12 21:40:37 +0530

    Made tokenizer.py the base class

--- LLM/tokenizer/tokenizer.py
+++ LLM/tokenizer/tokenizer.py
@@ -1,70 +1,9 @@
 class Tokenizer:
-    def __init__(self):
-        self.word_to_id = {}
-        self.id_to_word = {}
-
     def train(self, text):
-        tokens = text.split()
-        vocab = sorted(set(tokens))
-
-        self.word_to_id = {
-            word: i for i, word in enumerate(vocab)
-        }
-
-        self.id_to_word = {
-            i: word for word, i in self.word_to_id.items()
-        }
+        raise NotImplementedError
 
     def encode(self, text):
-        return [
-            self.word_to_id[word]
-            for word in text.split()
-        ]
+        raise NotImplementedError
 
     def decode(self, ids):
-        return " ".join(
-            self.id_to_word[i]
-            for i in ids
-        )
-
-with open("data.txt") as f:
-    text = f.read()
-
-tok = Tokenizer()
-
-tok.train(text)
-
-encoded = tok.encode("Hello AI!")
-
-print(encoded)
-
-print(tok.decode(encoded))
-
-class Tokenizer:
-    def __init__(self):
-        self.word_to_id = {}
-        self.id_to_word = {}
-
-    def train(self, text):
-        tokens = text.split()
-        vocab = sorted(set(tokens))
-
-        self.word_to_id = {
-            word: i for i, word in enumerate(vocab)
-        }
-
-        self.id_to_word = {
-            i: word for word, i in self.word_to_id.items()
-        }
-
-    def encode(self, text):
-        return [
-            self.word_to_id[word]
-            for word in text.split()
-        ]
-
-    def decode(self, ids):
-        return " ".join(
-            self.id_to_word[i]
-            for i in ids
-        )
+        raise NotImplementedError
